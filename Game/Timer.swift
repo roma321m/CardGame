d4a9e2331e69e67c78f2e267f6af5b861e0742a1
Roman <roma321m@gmail.com>
2022-04-24 10:51:56 +0200

    added timer class fixed the play button

---
+++ Game/Timer.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+class myTimer {
+    
+    var timer: Timer?
+    var runCount = 0
+    var numberOfIterations = 0
+    var timeInterval = 0.0
+    var repeats = true
+    var delegate: CallBack_Timer?
+    
+    init(delegate: CallBack_Timer, numberOfIterations: Int, timeInterval: Double, repeats: Bool){
+        self.delegate = delegate
+        self.numberOfIterations = numberOfIterations
+        self.timeInterval = timeInterval
+        self.repeats = repeats
+    }
+    
+    convenience init(delegate: CallBack_Timer, numberOfIterations: Int, timeInterval: Double){
+        self.init(delegate: delegate, numberOfIterations: numberOfIterations, timeInterval: timeInterval, repeats: true)
+    }
+    
+    convenience init(delegate: CallBack_Timer, numberOfIterations: Int) {
+        self.init(delegate: delegate, numberOfIterations: numberOfIterations, timeInterval: 1.0)
+    }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: repeats)
+    }
+    
+    @objc private func fireTimer() {
+        if runCount == numberOfIterations {
+            timer?.invalidate()
+            runCount = 0;
+        }else{
+            runCount += 1
+            delegate?.tickDetected(iter: runCount)
+        }
+    }
+}
+
+
+protocol CallBack_Timer {
+    func tickDetected(iter: Int)
+}
--- Game/ViewController.swift
+++ Game/ViewController.swift
@@ -2,12 +2,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var main_BTN_start: UIButton!
+    @IBOutlet weak var main_LBL_score_right: UILabel!
+    @IBOutlet weak var main_LBL_score_left: UILabel!
+    @IBOutlet weak var main_IMG_card_left: UIImageView!
+    @IBOutlet weak var main_IMG_card_right: UIImageView!
+    @IBOutlet weak var main_LBL_round_number: UILabel!
+    @IBOutlet weak var main_LBL_title: UILabel!
+    
+    var timer:myTimer?
+    var iterations = 5
+    var scoreLeft:Int = 0
+    var scoreRight:Int = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        timer = myTimer(delegate: self, numberOfIterations: iterations)
     }
+    
+    @IBAction func StartClikced(_ sender: Any) {
+        main_BTN_start.isHidden = true
+        timer?.startTimer()
+    }
+    
+}
 
-
+extension ViewController: CallBack_Timer {
+    
+    func tickDetected(iter: Int) {
+        print("iter: \(iter)")// for debugging
+        
+        if(iter == iterations){
+            // end game here - show res and get ready for next one
+        }else{
+            // roll cards and update the scores
+        }
+    }
+    
 }
 
